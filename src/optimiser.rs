d013941f8548389657464ec0be0e2a6ca2f24879
Atmolander <skylandersmimi@gmail.com>
2025-02-23 17:51:28 -0500

    added merging of offset operations

--- src/optimiser.rs
+++ src/optimiser.rs
@@ -7,13 +7,13 @@
 //! Then we make a "path" from the start position (0) to the end (in this case 2) which modifies
 //! all thoses cells with the least distance. In this situation distance is the number of '>' and '<'.
 
-use std::collections::HashSet;
+use std::{collections::HashSet, mem, ops::Range};
 
 /// Takes in a brainfuck program and removes redundant brainfuck operators by bulking them in `Operation`s.
 /// May break some behviour, like moving the pointer at the end of the program.
 pub fn optimise(bf: &str) -> String {
     // Parses the operations
-    let operations = parse_operations(&bf).0;
+    let mut operations = parse_operations(&bf).0;
 
     // -- Reordering and merging the operations --
     // Note how we didn't collect the operations into a hashmap as that would
@@ -24,7 +24,7 @@ pub fn optimise(bf: &str) -> String {
     // (Because of the lack of conditionals, we don't have to worry about one cell having causality on another)
     // NOTE: we will want to sort cell operations in increasing order if `start < end`
     // and in decreasing order if `start > end`
-
+    merge_offsets(&mut operations);
     operations_to_brainfuck(&operations)
 }
 
@@ -171,6 +171,107 @@ fn operations_to_brainfuck(ops: &[Operation]) -> String {
     }
 
     buf
+}
+
+/// Returns the range of valid positions the operator at `idx` can be inserted at.
+/// If the index is invalid returns an empty `0..0` range.
+fn operation_validity_range<'a, 'b>(ops: &'a [Operation<'b>], idx: usize) -> Range<usize> {
+    let Some(operator) = ops.get(idx) else { return 0..0 };
+    let mut range = idx..idx+1;
+
+    // searching forward
+    loop {
+        if range.end >= ops.len() {
+            break
+        }
+
+        let other = ops.get(range.end)
+            .expect("Since we know that range.start != ops.len(), we know we can be guarentied there is more");
+        if !operator.can_swap(other) {
+            break
+        }
+
+        range.end += 1;
+    }
+
+    // searching backward
+    loop {
+        if range.start == 0 {
+            break
+        }
+
+        let other = ops.get(range.start-1)
+            .expect("Since we know that range.start != 0, we know we can substract and not go oob");
+        if !operator.can_swap(other) {
+            break
+        }
+
+        range.start -= 1;
+    }
+
+    range
+}
+
+/// Merges offset operations togheter as much as fencing allows.
+fn merge_offsets<'a, 'b>(ops: &'a mut Vec<Operation<'b>>) {
+    // gets the indexes of all offsets,
+    // these won't change as we merge since we'll simply zero the recurence of merged offsets AND THEN delete them.
+    let offsets_indexes = ops.iter()
+        .enumerate()
+        .filter_map(|(i, op)| if let Operation::Offset { .. } = op {
+            Some(i)
+        } else {
+            None
+        }).collect::<Vec<_>>();
+
+    for offset_index in offsets_indexes {
+        let (self_cell, self_recurence) = {
+            let Operation::Offset { cell, recurence } = ops[offset_index] else { panic!("we know that it is offset") };
+            (cell, recurence)
+        };
+        // Getting where we can look for merge companions
+        let range = operation_validity_range(ops, offset_index);
+        
+        // search in front (excluding self ofc)
+        let other_offset_opt = ops[range.clone()].iter_mut()
+            .zip(range)
+            .find(|(op, i)| if let Operation::Offset { cell, recurence } = op {
+                if *i == offset_index {
+                    return false
+                }
+
+                if *cell != self_cell {
+                    return false
+                }
+
+                if *recurence == 0 {
+                    return false;
+                }
+
+                true
+            } else { false });
+
+        let other_offset = if let Some((other_offset, 0)) = other_offset_opt {
+            other_offset
+        } else {
+            continue;
+        };
+
+        // If we got here this means that we found a merge companion
+        let Operation::Offset { recurence, .. } = other_offset else { unreachable!() };
+        *recurence += self_recurence;
+
+        // we'll want to remove the recurence we just added to the original
+        let Operation::Offset { recurence, .. } = &mut ops[offset_index] else { unreachable!() };
+        *recurence = 0;
+    }
+
+    // Cleanup, we remove all the offsets we set to 0
+    let tmp_ops = mem::take(ops);
+    *ops = tmp_ops.into_iter().filter(|op| match op {
+        Operation::Offset { recurence: 0, ..} => false,
+        _ => true
+    }).collect();
 }
 
 // TODO: Zero operation would enable more optimisations.
@@ -451,6 +552,47 @@ mod tests {
         let new_output = inter.captured_output();
 
         assert_eq!(original_output, new_output);
+    }
+
+    #[test]
+    fn validity_range_works() {
+        // does nothing when there is nothing to do
+        let (ops, _) = parse_operations(">>,[>--+,]++>.<--<<");
+        assert_eq!(operation_validity_range(&ops, 0), 0..1);
+        assert_eq!(operation_validity_range(&ops, 2), 2..5);
+        assert_eq!(operation_validity_range(&ops, 1), 1..2);
+
+        let (ops, _) = parse_operations("+++[-]--");
+        assert!(!ops[0].can_swap(&ops[1]));
+        assert_eq!(operation_validity_range(&ops, 0), 0..1);
+    }
+
+    #[test]
+    fn merging_offset_optimisation() {
+        // does nothing when there is nothing to do
+        let (mut ops, _) = parse_operations("[>-  the harsh advice of those who love you rots within this hell<<]");
+        merge_offsets(&mut ops);
+        let string = operations_to_brainfuck(&ops);
+        assert_eq!(string, "[>-  the harsh advice of those who love you rots within this hell<<]");
+
+        // does nothing when there is nothing to do
+        let (mut ops, _) = parse_operations("+++[-]--");
+        assert_eq!(operation_validity_range(&ops, 0), 0..1);
+        merge_offsets(&mut ops);
+        let string = operations_to_brainfuck(&ops);
+        assert_eq!(string, "+++[-]--");
+
+        // does something when it needs to
+        let (mut ops, _) = parse_operations("+++>[-]<--");
+        assert_eq!(operation_validity_range(&ops, 0), 0..3);
+        merge_offsets(&mut ops);
+        let string = operations_to_brainfuck(&ops);
+        assert_eq!(string, "+>[-]");
+
+        let (mut ops, _) = parse_operations("++>>[-][-<<+>>]<<++");
+        merge_offsets(&mut ops);
+        let string = operations_to_brainfuck(&ops);
+        assert_eq!(string, "++++>>[-][-<<+>>]");
     }
 }
 
