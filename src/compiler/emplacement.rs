d98feede2f79860bedb159ebfd30403625ad0361
AtmolanderMimi <skylandersmimi@gmail.com>
2026-06-01 15:36:50 -0400

    removed indexing by list in emplacement

--- src/compiler/emplacement.rs
+++ src/compiler/emplacement.rs
@@ -2,8 +2,6 @@
 
 use std::mem;
 
-use either::Either;
-
 use crate::{compiler::expression::Expression, parser::{BinaryOperator as ParsedBinaryOperator, Expression as ParsedExpression, ExpressionItem as ParsedExpressionItem}};
 use thiserror::Error;
 use crate::{compiler::{CompilerError, scope::Scope, value::{Value, ValueType}}, parser::LanguageItem, use_as_parsed};
@@ -13,7 +11,7 @@ use_as_parsed!(EmplacementSubExpression);
 
 #[derive(Debug, Clone, PartialEq, Error)]
 pub enum EmplacementNormalizationError {
-    #[error("tried to index by {}, emplacement can only be index by number", invalid_type.name())]
+    #[error("tried to index by {}, emplacement can only be indexed by number", invalid_type.name())]
     InvalidIndexValueType {
         invalid_type: ValueType,
     },
@@ -32,7 +30,7 @@ pub struct NormalizedEmplacement {
 
 #[derive(Debug, Clone, PartialEq)]
 enum EmplacementOperation {
-    Index(Either<i32, Vec<i32>>),
+    Index(i32),
     Property(String),
 }
 
@@ -68,21 +66,8 @@ impl EmplacementSubExpression {
                 let value = rhs.evaluate(ctx)?;
 
                 // indexing by list
-                let operation = if let Value::List(vec) = value {
-                    // checks if the list is just numbers
-                    let invalid_type = vec.iter()
-                        .find(|v| !v.type_is_part_of(&ValueType::Number)).map(|v| v.type_of());
-                    if let Some(invalid_type) = invalid_type {
-                        let inner = EmplacementNormalizationError::InvalidIndexValueType { invalid_type };
-
-                        return Err(CompilerError::EmplacementNormalizationError { inner, expression: rhs.clone() });
-                    }
-
-                    let indexes = vec.iter().map(|v| v.as_number().unwrap()).collect();
-
-                    EmplacementOperation::Index(Either::Right(indexes))
-                } else if let Value::Number(number) = value {
-                    EmplacementOperation::Index(Either::Left(number))
+                let operation = if let Value::Number(number) = value {
+                    EmplacementOperation::Index(number)
                 } else {
                     let inner = EmplacementNormalizationError::InvalidIndexValueType { invalid_type: value.type_of() };
 
@@ -124,6 +109,12 @@ impl EmplacementSubExpression {
     }
 }
 
+impl NormalizedEmplacement {
+    //pub fn get_mut<'a>(&self, ctx: &'a mut Scope) -> &'a mut Value {
+    //    l
+    //}
+}
+
 #[cfg(test)]
 mod tests {
     use crate::parser::Pattern;
@@ -160,11 +151,16 @@ mod tests {
     }
 
     #[test]
-    fn index_normalized_emplacement() {
-        let normalized = normalized_from_str("&ident@[1,2]").unwrap();
+    fn index_number_is_valid_normalized_emplacement() {
+        let normalized = normalized_from_str("&ident@42").unwrap();
         assert_eq!(normalized.ident, "ident");
         assert_eq!(normalized.operations.len(), 1);
-        assert_matches!(normalized.operations[0], EmplacementOperation::Index(Either::Right(_)));
+        assert_eq!(normalized.operations[0], EmplacementOperation::Index(42));
+    }
+
+    #[test]
+    fn index_list_is_invalid_normalized_emplacement() {
+        normalized_from_str("&ident@[1,2]").unwrap_err();
     }
 
     #[test]
@@ -182,7 +178,7 @@ mod tests {
 
     #[test]
     fn combined_operator_precedence_respected_normalized_emplacement() {
-        let normalized = normalized_from_str("&ident.\"name\"@[1,2,3]").unwrap();
+        let normalized = normalized_from_str("&ident.\"name\"@3").unwrap();
         assert_eq!(normalized.ident, "ident");
         assert_eq!(normalized.operations.len(), 2);
         assert_eq!(normalized.operations[0], EmplacementOperation::Property("name".to_string()));
@@ -191,7 +187,7 @@ mod tests {
 
     #[test]
     fn combined_operator_precedence_respected2_normalized_emplacement() {
-        let normalized = normalized_from_str("&(ident@[1,2,3]).\"len\"").unwrap();
+        let normalized = normalized_from_str("&(ident@3).\"len\"").unwrap();
         assert_eq!(normalized.ident, "ident");
         assert_eq!(normalized.operations.len(), 2);
         assert_matches!(normalized.operations[0], EmplacementOperation::Index(_));
@@ -203,6 +199,6 @@ mod tests {
         let normalized = normalized_from_str("&ident@(\"hello\"+\", world!\").\"len\"").unwrap();
         assert_eq!(normalized.ident, "ident");
         assert_eq!(normalized.operations.len(), 1);
-        assert_eq!(normalized.operations[0], EmplacementOperation::Index(Either::Left(13)));
+        assert_eq!(normalized.operations[0], EmplacementOperation::Index(13));
     }
 }
